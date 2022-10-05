afd220a32a14bac8302e804eb695110c31c0ddbf
Srajan Agrawal <91273821+SrajanAgrawal@users.noreply.github.com>
2022-10-05 11:48:35 +0530

    binarySearchAlgorithm.cpp

---
+++ BinarySearchAlgo.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// code by srajan
+
+// Function for searching an element using binary search
+
+int binarySearch(vector<int> v, int To_Find)
+{
+    int lo = 0, hi = v.size() - 1;
+    int mid;
+    while (hi - lo > 1) {
+        int mid = (hi + lo) / 2;
+        if (v[mid] < To_Find) {
+            lo = mid + 1;
+        }
+        else {
+            hi = mid;
+        }
+    }
+    if (v[lo] == To_Find) {
+        cout << "Found"
+             << " At Index " << lo << endl;
+    }
+    else if (v[hi] == To_Find) {
+        cout << "Found"
+             << " At Index " << hi << endl;
+    }
+    else {
+        cout << "Not Found" << endl;
+    }
+}
+ 
+// Main function 
+int main()
+{
+    vector<int> v = { 1, 3, 4, 5, 6 };
+    int To_Find = 1;
+    binarySearch(v, To_Find);
+    To_Find = 6;
+    binarySearch(v, To_Find);
+    To_Find = 10;
+    binarySearch(v, To_Find);
+    return 0;
+}
+
+
+
+
+
+{
+
+Complexity Analysis: 
+    Time Complexity => O(log n)
+    Space Complexity => O(1)
+      
+}
